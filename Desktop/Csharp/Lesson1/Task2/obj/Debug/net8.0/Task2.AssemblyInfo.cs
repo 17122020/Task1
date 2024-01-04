6e01bd74bd89037a594240171bb4020bdd7c12f0
Karyna Alexandrovich <karynaalex2020@mail.ru>
2024-01-04 17:24:49 +0300

    Урок1 Task2

--- Desktop/Csharp/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Desktop/Csharp/Lesson1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31570d57bb2470b86daab5085107df346e8f7441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0bcf882104dc0c1f801f8d25344ca65e68db22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
